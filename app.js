3940b1bb4d3f6dc251d7373f109731d9562e7e4e
Abdelouahed Ait-essaih <abdelouahed-maroc@live.fr>
2022-07-29 18:05:47 +0100

    init routes

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ const express = require("express");
 
 const app = express();
 
+//routes
+app.get('/',(req,res,next)=> {
+   res.json({
+       message: "Welcome to the homepage"
+   });
+});
+
 module.exports = app;
